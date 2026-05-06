d26c088ca79642d439870149e62e7fa03d1929ba
FabioJrDeEV <fj453578@gmail.com>
2026-05-06 08:45:41 -0300

    feat: update layout.tsx to use Tailwind classes

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Passo Fino | Loja de Sapatos",
+  description: "Loja de sapatos com novidades, modelos femininos e masculinos."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
