729c39c6566f69248e5c5af532e717fe9650a2fe
FabioJrDeEV <fj453578@gmail.com>
2026-05-29 10:40:18 -0300

    feat: backend folder added to the project.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
 import type { ReactNode } from "react";
 
 export default function Layout({
@@ -10,7 +8,7 @@ export default function Layout({
   children: ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="scroll-smooth">
       <body className="min-h-screen flex flex-col">
         <CartProvider>
           <main className="flex-grow overflow-x-hidden">{children}</main>
